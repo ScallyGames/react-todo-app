d95c4e561cf02b2abddd3270f651881cc6555ae0
Pascal Blessing <pblessing.mmt-b2017@fh-salzburg.ac.at>
2021-11-27 00:49:04 +0100

    Add material ui

--- client/src/components/CreateTodoListItem.jsx
+++ client/src/components/CreateTodoListItem.jsx
@@ -1,5 +1,7 @@
+import './CreateTodoListItem.css'
 import * as React from 'react';
 import { NewTodo } from '../model/new-todo.model'
+import { Button, Box, TextField } from '@mui/material'
 
 export class CreateTodoListItem extends React.Component {
 
@@ -14,40 +16,26 @@ export class CreateTodoListItem extends React.Component {
     }
 
     create = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
         fetch(`http://localhost:3001/api/todos`, {
             'method': 'POST',
-            'body': JSON.stringify(new NewTodo(this.state.title, this.state.description)),
+            'body': JSON.stringify(new NewTodo(data.get('title'), data.get('description'))),
             'headers': {
                 'Content-Type': 'application/json'
             }
         })
             .then(response => {
                 this.props.onCreated();
+                event.target.reset();
             });
-
-        event.preventDefault();
     };
 
-    handleInputChange = (event) => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-        this.setState({
-          [name]: value    
-        });
-    }
-
     render() {
-        return <form onSubmit={this.create}>
-            <label>
-                Title:
-                <input name="title" type="text" value={this.state.title} onChange={this.handleInputChange} />
-            </label>
-            <label> 
-                Description:
-                <input name="description" type="text" value={this.state.description} onChange={this.handleInputChange} />
-            </label>
-            <button type="submit">Create</button>
-        </form>;
+        return <Box component="form" onSubmit={this.create} noValidate>
+            <div><TextField required id="title" label="Title" name="title" /></div>
+            <div><TextField required id="description" label="Description" name="description" /></div>
+            <div><Button type="submit" variant="contained">Create</Button></div>
+        </Box>;
     }
 }
--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,10 +1,35 @@
 import './TodoList.css';
 import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 import { Todo } from '../model/todo.model';
-import { TodoListItem } from './TodoListItem';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import Button from '@mui/material/Button';
 import { CreateTodoListItem } from './CreateTodoListItem';
 
 export class TodoList extends React.Component {
+
+    columns = [
+        { field: 'title', headerName: 'Title' },
+        { field: 'description', headerName: 'Description' },
+        {
+            field: "",
+            headerName: "",
+            sortable: false,
+            width: 80,
+            disableClickEventBubbling: true,
+            renderCell: (params) => {
+                return (
+                    <div>
+                        <Button type="submit" variant="contained"
+                          onClick={this.delete.bind(this, params.id)}
+                        >
+                            <DeleteForeverIcon />
+                        </Button>
+                    </div>
+                );
+            }
+        },
+    ];
 
     constructor(props) {
         super(props);
@@ -18,41 +43,40 @@ export class TodoList extends React.Component {
         this.loadData();
     }
 
+    delete = (id) => {
+        fetch(`http://localhost:3001/api/todos/${id}`, {
+            "method": "DELETE",
+        })
+            .then(response => {
+                this.loadData();
+            });
+    };
+
     loadData = () => {
         fetch("http://localhost:3001/api/todos")
-        .then(res => res.json())
-        .then(result => result.map(x => new Todo(x.id, x.title, x.description, x.done)))
-        .then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    todos: result
-                });
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        );
-    }
+            .then(res => res.json())
+            .then(result => result.map(x => new Todo(x.id, x.title, x.description, x.done)))
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        todos: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+    };
 
     render() {
-        return <div>
-            <table cellPadding="8" cellSpacing="0">
-                <thead>
-                    <tr>
-                        <td>Title</td>
-                        <td>Description</td>
-                        <td></td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.todos.map(x => <TodoListItem key={x.id} item={x} onDeleted={this.loadData} />)}
-                </tbody>
-            </table>
+        return <div className="todo-list">
             <CreateTodoListItem onCreated={this.loadData} />
+            <DataGrid rows={this.state.todos} columns={this.columns} pageSize={10} disableSelectionOnClick={true} isCellEditable={false} isRowSelectable={false}>
+            </DataGrid>
         </div>;
     }
 }
